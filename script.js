4b2c74ecfcc5d1f7f377a81e59841825548bf6dd
jdittert <jondittert@gmail.com>
2023-02-14 10:35:18 -0500

    Code pencilOn and pencilOff functions

--- script.js
+++ script.js
@@ -8,7 +8,26 @@ function createCanvas() {
             pixelCount++;
         }
     }
-    console.log(pixelCount);
+    console.log(pixelCount);    
 }
 
-document.addEventListener("DOMContentLoaded", function() { createCanvas(); });
+function fillIn() {
+    this.classList.add("filled");
+}
+
+function pencilOn() {   
+    const pixels = document.querySelectorAll("div.pixel");       
+    pixels.forEach(pixel => pixel.addEventListener("mouseover", fillIn));
+    canvas.addEventListener("mouseup", pencilOff);
+}
+
+function pencilOff() {
+    const pixels = document.querySelectorAll("div.pixel");    
+    pixels.forEach(pixel => pixel.removeEventListener("mouseover", fillIn));
+}
+
+document.addEventListener("DOMContentLoaded", createCanvas);
+const canvas = document.getElementById("canvas");
+canvas.addEventListener("mousedown", pencilOn);
+const pixels = document.querySelectorAll("div.pixel");
+pixels.forEach(pixel => pixel.addEventListener("click", fillIn));
