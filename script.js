81307db993904b45de32b6318f00031d3cea3ef4
jdittert <jondittert@gmail.com>
2023-02-15 12:04:11 -0500

    Code resize function

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
-function createCanvas() {    
-    for (i = 0; i < 256; i++) {        
-        newPixel = document.createElement("div");
-        newPixel.classList.add("pixel");
+const size = 16;
+
+function createCanvas(size) {
+    let width = 320 / size; 
+    for (i = 0; i < size * size; i++) {                 
+        newPixel = document.createElement("div");   
+        newPixel.classList.add("pixel");   
+        newPixel.style.width = width + "px";        
         document.getElementById("canvas").appendChild(newPixel);        
-        }        
+        }    
+    console.log(width);    
 }
 
 function fillIn() {
@@ -31,10 +36,28 @@ function clearCanvas() {
     pixels.forEach(pixel => pixel.classList.remove("filled"));
 }
 
-document.addEventListener("DOMContentLoaded", createCanvas);
+function resizeCanvas() {
+    let size = prompt("Enter the number of tiles per side (max 100).");
+    console.log(size);
+    if (size > 0 && size < 101) {
+        size = +size;
+        clearCanvas();        
+        canvas.replaceChildren();
+        createCanvas(size);
+    } else {
+        alert("Enter a number between 0-100. Canvas has been reset to 16x16");
+        clearCanvas();
+        canvas.replaceChildren();
+        createCanvas(16);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function() { createCanvas(size); });
 const canvas = document.getElementById("canvas");
 canvas.addEventListener("mouseenter", pencilClick);
 canvas.addEventListener("mousedown", pencilOn);
 const clear = document.getElementById("clear");
 clear.addEventListener("click", clearCanvas);
+const resize = document.getElementById("resize");
+resize.addEventListener("click", resizeCanvas);
 
