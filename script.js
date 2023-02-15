ecafff6973d60c8ab6f8e7f8878843c3fe1c0e7d
jdittert <jondittert@gmail.com>
2023-02-15 16:08:22 -0500

    Add comments to index and script pages

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+// Set initial 16x16 size for canvas
 let size = 16;
+
+// Create initial canvas
+document.addEventListener("DOMContentLoaded", function() { createCanvas(size); });
 
 function createCanvas(size) {
     let width = 320 / size; 
@@ -7,10 +11,15 @@ function createCanvas(size) {
         newPixel.classList.add("pixel");   
         newPixel.style.width = width + "px";        
         document.getElementById("canvas").appendChild(newPixel);        
-        }    
-    console.log(width);    
+        }        
 }
 
+// Listen for mouse on canvas
+const canvas = document.getElementById("canvas");
+canvas.addEventListener("mouseenter", pencilClick);
+canvas.addEventListener("mousedown", pencilOn);
+
+// Fill pixels when clicked or moused over
 function fillIn() {
     this.classList.add("filled");
 }
@@ -31,16 +40,23 @@ function pencilClick() {
     pixels.forEach(pixel => pixel.addEventListener("mousedown", fillIn));
 }
 
+// Clear canvas button functionality
+const clear = document.getElementById("clear");
+clear.addEventListener("click", clearCanvas);
+
 function clearCanvas() {
     const pixels = document.querySelectorAll("div.pixel");
     pixels.forEach(pixel => pixel.classList.remove("filled"));
 }
 
+// Resize canvas button functionality
+const resize = document.getElementById("resize");
+resize.addEventListener("click", getNewSize);
+
 function getNewSize() {
     let newSize = prompt("Enter the number of tiles per side (max 100).");    
-    if (newSize > 0 && newSize < 101) {
-        size = newSize;
-        resizeCanvas(size);
+    if (newSize > 0 && newSize < 101) {        
+        resizeCanvas(newSize);
     } else {
         alert("Enter a number between 0-100. Canvas has been reset to 16x16");
         resizeCanvas(16);
@@ -53,12 +69,8 @@ function resizeCanvas(size) {
     createCanvas(size);
 }
 
-document.addEventListener("DOMContentLoaded", function() { createCanvas(size); });
-const canvas = document.getElementById("canvas");
-canvas.addEventListener("mouseenter", pencilClick);
-canvas.addEventListener("mousedown", pencilOn);
-const clear = document.getElementById("clear");
-clear.addEventListener("click", clearCanvas);
-const resize = document.getElementById("resize");
-resize.addEventListener("click", getNewSize);
+
+
+
+
 
